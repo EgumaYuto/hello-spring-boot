88f99ef44e3dd92d923776d676a5e132d93db694
yuto.eguma <yuto.eguma@gmail.com>
2025-02-10 08:53:16 +0900

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.21"
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.flywaydb.flyway") version "11.3.1"
 }
 
 group = "org.example"
@@ -17,6 +18,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
+
+    implementation("org.flywaydb:flyway-core:11.3.1")
+    implementation("mysql:mysql-connector-java:8.0.33")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
