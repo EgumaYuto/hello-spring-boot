953e3d88527d9258d86aaeca57dbb96502009bac
yuto.eguma <yuto.eguma@gmail.com>
2025-02-13 18:52:47 +0900

    eguma/#8 install flyway and mysql connect settings to enable developers to migrate database

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,15 @@
+buildscript {
+    dependencies {
+        classpath("org.flywaydb:flyway-mysql:11.3.1")
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.9.21"
     kotlin("plugin.spring") version "1.9.21"
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.flywaydb.flyway") version "11.3.1"
 }
 
 group = "org.example"
@@ -18,7 +25,18 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
+    implementation("org.flywaydb:flyway-core:11.3.1")
+    implementation("org.flywaydb:flyway-mysql:11.3.1")
+
+    runtimeOnly("mysql:mysql-connector-java:8.0.33")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+flyway {
+    url = "jdbc:mysql://127.0.0.1:3306/hellodb?useSSL=false"
+    user = "hellouser"
+    password = "hellopassword"
 }
 
 tasks.test {
